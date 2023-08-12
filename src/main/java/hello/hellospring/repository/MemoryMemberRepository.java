d706f2be039769bbb6e2793b2413469c7e2e1629
yelo-o <flyordig@gmail.com>
2023-08-13 00:23:02 +0900

    test

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -1,9 +1,11 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
+@Repository
 public class MemoryMemberRepository implements MemberRepository {
     private static Map<Long, Member> store = new HashMap<>();
     private static long sequence = 0L; //sequence : 0, 1, 2로 키값 생성해 주는 변수
