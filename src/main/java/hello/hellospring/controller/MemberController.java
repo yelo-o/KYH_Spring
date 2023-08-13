9a9cbbef573a73d0d3e164ce0f94dc229202aa8e
yelo-o <flyordig@gmail.com>
2023-08-13 09:57:55 +0900

    Spring Bean & Dependency Injection

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class MemberController {
-    private final MemberService memberService;
+    private MemberService memberService;
 
     @Autowired
     public MemberController(MemberService memberService) {
